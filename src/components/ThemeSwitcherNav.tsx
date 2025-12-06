54ef4e8192b2053539366e77d3aeccb1a9d3d0a0
Ian Lintner <lintner.ian@gmail.com>
2025-12-05 21:46:30 -0600

    refactor: format dynamic import of ThemeSwitcher for improved readability

--- src/components/ThemeSwitcherNav.tsx
+++ src/components/ThemeSwitcherNav.tsx
@@ -4,10 +4,14 @@ import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
 // Dynamically import ThemeSwitcher to prevent SSR/SSG issues
-const ThemeSwitcher = dynamic(() => import("./ThemeSwitcher").then(mod => ({ default: mod.ThemeSwitcher })), {
-  ssr: false,
-  loading: () => <div className="w-32 h-10" />,
-});
+const ThemeSwitcher = dynamic(
+  () =>
+    import("./ThemeSwitcher").then((mod) => ({ default: mod.ThemeSwitcher })),
+  {
+    ssr: false,
+    loading: () => <div className="w-32 h-10" />,
+  },
+);
 
 /**
  * Wrapper component for ThemeSwitcher with dynamic import
