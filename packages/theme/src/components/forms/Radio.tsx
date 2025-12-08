4363d39041f8c5ec1180e395e60fb37ea5910332
Ian Lintner <lintner.ian@gmail.com>
2025-12-08 15:34:45 -0600

    feat: add Radio component with props and styles

---
+++ packages/theme/src/components/forms/Radio.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { forwardRef } from "react";
+import { cn } from "../../utils/cn";
+
+export interface RadioProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {}
+
+/**
+ * Radio
+ *
+ * Styled radio button.
+ */
+export const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="radio"
+        className={cn(
+          "h-4 w-4 shrink-0 rounded-full border border-input bg-background/80",
+          "text-primary shadow-sm transition-all",
+          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary",
+          "focus-visible:ring-offset-2 focus-visible:ring-offset-background",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          "accent-primary",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+
+Radio.displayName = "Radio";
