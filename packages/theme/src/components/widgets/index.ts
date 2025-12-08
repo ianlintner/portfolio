158bcf238423754bcafa4885c7b52328d2f9c49d
Ian Lintner <lintner.ian@gmail.com>
2025-12-08 16:06:02 -0600

    feat: create index file for widget components to streamline exports

---
+++ packages/theme/src/components/widgets/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Widget Components
+ */
+
+export { StatCard, type StatCardProps } from "./StatCard";
+export { ChartCard, type ChartCardProps } from "./ChartCard";
+export { ActivityFeed, type ActivityFeedProps, type ActivityItem } from "./ActivityFeed";
+export { QuickActions, type QuickActionsProps, type QuickAction } from "./QuickActions";
