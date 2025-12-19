dcf14188921bf90bd0de8e166a0f5d48e368ecf1
Ian Lintner <lintner.ian@gmail.com>
2025-12-19 16:56:38 -0600

    feat: add UIScene class for displaying score and lives in the game

---
+++ src/game/scenes/UIScene.ts
@@ -0,0 +1,15 @@
+import { Scene } from 'phaser';
+
+export class UIScene extends Scene {
+    private scoreText!: Phaser.GameObjects.Text;
+    private livesText!: Phaser.GameObjects.Text;
+
+    constructor() {
+        super('UIScene');
+    }
+
+    create() {
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', color: '#fff' });
+        this.livesText = this.add.text(16, 50, 'Lives: 9', { fontSize: '32px', color: '#fff' });
+    }
+}
