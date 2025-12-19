97591a16cee83eadf53180f6f49fb8f43cc6ca62
Ian Lintner <lintner.ian@gmail.com>
2025-12-19 16:56:47 -0600

    feat: add Victory scene to display win message and restart option

---
+++ src/game/scenes/Victory.ts
@@ -0,0 +1,26 @@
+import { Scene } from 'phaser';
+
+export class Victory extends Scene {
+    constructor() {
+        super('Victory');
+    }
+
+    create() {
+        const { width, height } = this.scale;
+
+        this.add.text(width / 2, height / 2, 'You Win!', {
+            fontSize: '64px',
+            color: '#00ff00'
+        }).setOrigin(0.5);
+
+        this.add.text(width / 2, height / 2 + 100, 'Click to Play Again', {
+            fontSize: '32px',
+            color: '#ffffff'
+        })
+        .setOrigin(0.5)
+        .setInteractive()
+        .on('pointerdown', () => {
+            this.scene.start('MainMenu');
+        });
+    }
+}
